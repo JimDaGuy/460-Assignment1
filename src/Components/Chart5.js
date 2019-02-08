180407fe3b88e07e51203957f5ad2cb9d1ee3905
James Digrazia <jtd2401@rit.edu>
2019-02-08 10:05:08 -0500

    Add some styling to the page

--- src/Components/Chart5.js
+++ src/Components/Chart5.js
@@ -15,7 +15,6 @@ class Chart4 extends Component {
       };
     })
       .then(csvData => {
-        // this.visualizeCSV(csvData);
         this.svgCSV(csvData);
       })
       .catch(error => {
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,17 +12,80 @@ class Home extends Component {
   render() {
     return (
       <div className={homeStyles.container}>
-        <h1>FakeDroid App Chart Story</h1>
-        <h2>Introduction</h2>
-        <p>This is a story about app downloads...</p>
-        <h2>What We Found</h2>
-        <Chart1 />
-        <Chart2 />
-        <Chart3 />
-        <Chart4 />
-        <Chart5 />
-        <h2>Conclusions</h2>
-        <p>The data shows...</p>
+        <h1 className={homeStyles.h1}>FakeDroid App Chart Story</h1>
+        <h2 className={homeStyles.h2}>Introduction</h2>
+        <p className={homeStyles.p}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          faucibus sapien molestie mauris porta convallis eget at justo. Proin
+          finibus orci in lobortis elementum. Sed tincidunt augue eu tortor
+          imperdiet, in dignissim ex tincidunt.
+        </p>
+        <h2 className={homeStyles.h2}>What We Found</h2>
+        <h3 className={homeStyles.h3}>1. Overall Downloads</h3>
+        <hr className={homeStyles.hr} />
+        <p className={homeStyles.p}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          faucibus sapien molestie mauris porta convallis eget at justo. Proin
+          finibus orci in lobortis elementum. Sed tincidunt augue eu tortor
+          imperdiet, in dignissim ex tincidunt.
+        </p>
+        <div className={homeStyles.chartContainer}>
+          <Chart1 className={homeStyles.chart} />
+        </div>
+        <h3 className={homeStyles.h3}>2. Average Rating</h3>
+        <hr className={homeStyles.hr} />
+        <p className={homeStyles.p}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          faucibus sapien molestie mauris porta convallis eget at justo. Proin
+          finibus orci in lobortis elementum. Sed tincidunt augue eu tortor
+          imperdiet, in dignissim ex tincidunt.
+        </p>
+        <div className={homeStyles.chartContainer}>
+          <Chart2 className={homeStyles.chart} />
+        </div>
+        <h3 className={homeStyles.h3}>3. 30 Day Retention</h3>
+        <hr className={homeStyles.hr} />
+        <p className={homeStyles.p}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          faucibus sapien molestie mauris porta convallis eget at justo. Proin
+          finibus orci in lobortis elementum. Sed tincidunt augue eu tortor
+          imperdiet, in dignissim ex tincidunt.
+        </p>
+        <div className={homeStyles.chartContainer}>
+          <Chart3 className={homeStyles.chart} />
+        </div>
+        <h3 className={homeStyles.h3}>4. Overall Downloads / Average Rating</h3>
+        <hr className={homeStyles.hr} />
+        <p className={homeStyles.p}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          faucibus sapien molestie mauris porta convallis eget at justo. Proin
+          finibus orci in lobortis elementum. Sed tincidunt augue eu tortor
+          imperdiet, in dignissim ex tincidunt.
+        </p>
+        <div className={homeStyles.chartContainer}>
+          <Chart4 className={homeStyles.chart} />
+        </div>
+        <h3 className={homeStyles.h3}>
+          5. Overall Downloads / 30 Day Retention
+        </h3>
+        <hr className={homeStyles.hr} />
+        <p className={homeStyles.p}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          faucibus sapien molestie mauris porta convallis eget at justo. Proin
+          finibus orci in lobortis elementum. Sed tincidunt augue eu tortor
+          imperdiet, in dignissim ex tincidunt.
+        </p>
+        <div className={homeStyles.chartContainer}>
+          <Chart5 className={homeStyles.chart} />
+        </div>
+        <h3 className={homeStyles.h3}>Conclusions</h3>
+        <hr className={homeStyles.hr} />
+        <p className={homeStyles.p}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          faucibus sapien molestie mauris porta convallis eget at justo. Proin
+          finibus orci in lobortis elementum. Sed tincidunt augue eu tortor
+          imperdiet, in dignissim ex tincidunt.
+        </p>
       </div>
     );
   }
